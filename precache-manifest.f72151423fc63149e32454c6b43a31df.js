2f6e355dc05c3df34fa2641d8b6d0fc7e983722b
Sharma10 <PSharma10@ad.harman.com>
2020-08-05 23:33:52 +0530

    Updates

---
+++ precache-manifest.f72151423fc63149e32454c6b43a31df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e2be4b511f2ab28c5c8c762d819de9",
+    "url": "/react-jira-app-deploy/index.html"
+  },
+  {
+    "revision": "e586d88db3a0196bd2ae",
+    "url": "/react-jira-app-deploy/static/css/main.e28fe001.chunk.css"
+  },
+  {
+    "revision": "db86c4bca98082040219",
+    "url": "/react-jira-app-deploy/static/js/2.1afc551f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-jira-app-deploy/static/js/2.1afc551f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e586d88db3a0196bd2ae",
+    "url": "/react-jira-app-deploy/static/js/main.77d86d8f.chunk.js"
+  },
+  {
+    "revision": "a620e7a4072fe532c1f2",
+    "url": "/react-jira-app-deploy/static/js/runtime-main.6b3b9bae.js"
+  }
+]);
